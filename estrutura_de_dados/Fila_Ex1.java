9e1edbe146728abb95b53f17016dbbb7160df624
alinesoglia <alinepsoglia@gmail.com>
2023-03-23 13:58:42 -0300

    Enviando exercícios de fila e pilha

---
+++ estrutura_de_dados/Fila_Ex1.java
@@ -0,0 +1,65 @@
+package estrutura_de_dados;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Fila_Ex1 {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
+		Queue<String> fila = new LinkedList<String>();
+
+		while (true) {
+			System.out.println("********************************************************");
+			System.out.println("                                                        ");
+			System.out.println("                                                        ");
+			System.out.println("             1 - Adicionar Cliente na Fila              ");
+			System.out.println("             2 - Listar todos os Clientes               ");
+			System.out.println("             3 - Retirar Cliente da Fila                ");
+			System.out.println("             0 - Sair                                   ");
+			System.out.println("                                                        ");
+			System.out.println("                                                        ");
+			System.out.println("********************************************************");
+			System.out.println("Entre com a opção desejada: ");
+			int opcao = leia.nextInt();
+
+			if (opcao == 0) {
+				System.out.println("\nPrograma finalizado!");
+				System.exit(0);
+
+			}
+
+			switch (opcao) {
+
+			case 1:
+
+				System.out.println("\nAdicionar Cliente na Fila: ");
+				leia.nextLine();
+				String novoCliente = leia.nextLine();
+				fila.add(novoCliente);
+				System.out.println("\nFila: "+fila);
+				System.out.println("\nCliente adicionado!");
+
+				break;
+
+			case 2:
+				System.out.println("\nListar todos os clientes: " + fila);
+				break;
+
+			case 3:
+				System.out.println(fila.poll()+"\nO cliente foi chamado!");
+				System.out.println("Fila: "+fila);
+				break;
+
+			default:
+				System.out.println("\nOpção Inválida!!!");
+				break;
+			}
+		}
+
+	}
+
+}
---
+++ estrutura_de_dados/Pilha_Ex1.java
@@ -0,0 +1,64 @@
+package estrutura_de_dados;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Pilha_Ex1 {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
+		Stack<String> pilha = new Stack<String>();
+
+		while (true) {
+			System.out.println("********************************************************");
+			System.out.println("                                                        ");
+			System.out.println("                                                        ");
+			System.out.println("             1 - Adicionar Livro na Pilha               ");
+			System.out.println("             2 - Listar todos os Livros                 ");
+			System.out.println("             3 - Retirar Livro da Pilha                 ");
+			System.out.println("             0 - Sair                                   ");
+			System.out.println("                                                        ");
+			System.out.println("                                                        ");
+			System.out.println("********************************************************");
+			System.out.println("Entre com a opção desejada: ");
+			int opcao = leia.nextInt();
+
+			if (opcao == 0) {
+				System.out.println("\nPrograma finalizado!");
+				System.exit(0);
+
+			}
+
+			switch (opcao) {
+
+			case 1:
+
+				System.out.println("\nAdicionar Livro na Pilha: ");
+				leia.nextLine();
+				String novoLivro = leia.nextLine();
+				pilha.push(novoLivro);
+				System.out.println("\nPilha: " + pilha);
+				System.out.println("\nLivro adicionado!");
+
+				break;
+
+			case 2:
+				System.out.println("\nLista de livros na pilha: " + pilha);
+				break;
+
+			case 3:
+				System.out.println(pilha.pop() + " foi retirado da pilha!");
+				System.out.println("\nPilha: " + pilha);
+				break;
+
+			default:
+				System.out.println("\nOpção Inválida!!!");
+				break;
+			}
+		}
+
+	}
+
+}
